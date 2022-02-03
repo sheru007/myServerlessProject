c75766d30c082a6f24d9ccc562436aeaa34e0db9
sheru007 <jnvh1233sheru@gmail.com>
2022-02-03 11:16:31 +0530

    created curtom hook for event validation

--- lambdas/common/hooks.js
+++ lambdas/common/hooks.js
@@ -7,6 +7,57 @@ const withHooks = useHooks({
 })
 // parseEvent : already parse the event so no need of JSON.parse and event.pathParameter is already a object
 
+const hooksWithValidation = ({bodySchema, pathSchema}) => {
+
+    return useHooks({
+        before: [logEvent, parseEvent, validateEventBody, validatePath],
+        after: [],
+        onError: [handleUnexpectedError]
+    },{
+        bodySchema,
+        pathSchema
+    })
+}
+
+const validateEventBody = async state => {
+    const { bodySchema } = state.config;
+
+    if(!bodySchema) {
+        throw Error('missing the required body schema')
+    }
+
+    try {
+        const { event } = state;
+        await bodySchema.validate(event.body, { strict: true })
+    } catch (error) {
+        console.log('validation error of event.body : ', error)
+        state.exit = true;
+        state.response = { statusCode: 400, body: JSON.stringify({error: error.message}) }
+    }
+
+    return state;
+}
+
+const validatePath = async state => {
+    const { pathSchema } = state.config;
+
+    if(!pathSchema) {
+        throw Error('missing the required path schema')
+    }
+
+    try {
+        const { event } = state;
+        await pathSchema.validate(event.pathParameters, { strict: true })
+    } catch (error) {
+        console.log('validation error of event.pathParameters : ', error)
+        state.exit = true;
+        state.response = { statusCode: 400, body: JSON.stringify({error: error.message}) }
+    }
+
+    return state;
+}
+
 module.exports = {
-    withHooks
+    withHooks,
+    hooksWithValidation
 }
--- lambdas/endpoints/createPlayerScore.js
+++ lambdas/endpoints/createPlayerScore.js
@@ -1,8 +1,18 @@
 const Responses = require('../common/API_Responses')
 const Dynamo = require('../common/Dynamo')
-const {withHooks} = require('../common/hooks')
+const {hooksWithValidation} = require('../common/hooks')
+const yup = require('yup')
 
 const tableName = process.env.tableName
+
+const bodySchema = yup.object().shape({
+    name: yup.string().required(),
+    score: yup.number().required()
+})
+
+const pathSchema = yup.object().shape({
+    ID: yup.string().required()
+})
 
 const handler = async (event) => {
 
@@ -24,5 +34,5 @@ const handler = async (event) => {
     return Responses._200({newUser})
 }
 
-exports.handler = withHooks(handler)
+exports.handler = hooksWithValidation({bodySchema,pathSchema})(handler)
 //https://dv5fwk1589.execute-api.us-east-1.amazonaws.com/dev/create-player-score/{ID}
