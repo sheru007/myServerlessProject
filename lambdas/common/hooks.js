5615929ac3cac4bf77f5a8a6feaff867152ed9e5
sheru007 <jnvh1233sheru@gmail.com>
2022-02-03 11:38:12 +0530

    hooks documentation done

--- lambdas/common/hooks.js
+++ lambdas/common/hooks.js
@@ -1,9 +1,16 @@
 const { useHooks, logEvent, parseEvent, handleUnexpectedError } = require('lambda-hooks')
 
 const withHooks = useHooks({
+    // start -->
     before: [logEvent, parseEvent],
+
+    // lambda function is invoked now...
+
     after: [],
+    //        Finish -->|
+
     onError: [handleUnexpectedError]
+    // Finish if errors -->|
 })
 // parseEvent : already parse the event so no need of JSON.parse and event.pathParameter is already a object
 
@@ -18,6 +25,31 @@ const hooksWithValidation = ({bodySchema, pathSchema}) => {
         pathSchema
     })
 }
+
+// interface State {
+//     event: Event // AWS lambda event
+//     context: Context // AWS lambda context
+//     exit: boolean // Set to true to quit execution early
+//     response?: Response // This will contain the response from your lambda after it has been executed. Also this will be returned when exit is true
+//     error?: Error // If there's an unhandled exception, it will be attached here & your onError handlers will be invoked
+//     config: any // Config object to provide extra things to your hooks at the point of execution e.g. you might want to pass a logger into logEvent
+// }
+
+// const logEvent = async state => {
+//     console.log(`received event: ${state.event}`)
+
+//     return state
+// }
+
+// const parseEventBody = async state => {
+//     const { event } = state
+
+//     if (typeof event.body === 'string') {
+//         state.event.body = JSON.parse(event.body)
+//     }
+
+//     return state
+// }
 
 const validateEventBody = async state => {
     const { bodySchema } = state.config;
